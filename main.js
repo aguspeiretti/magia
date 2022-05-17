9a663b55ac836210ea81e64143c2c4143fe972dc
aguspeiretti <aguspeiretti@gmail.com>
2022-05-17 15:43:43 -0300

    subia2

--- main.js
+++ main.js
@@ -20,51 +20,51 @@ body.addEventListener("click", () => {
 function extraerDato() {
   var horo = document.getElementById("signo").value;
 
-  if (horo == "aries") {
+  if (horo == "Aries") {
     alert(
       "grandes cosas, simplemente porque te sentirás bastante en paz contigo misma y con tu entorno."
     );
-  } else if (horo == "tauro") {
+  } else if (horo == "Tauro") {
     alert(
       "Aprovecha el dia saliendo con amigos y disfrutando a tope de este día de ocio"
     );
-  } else if (horo == "geminis") {
+  } else if (horo == "Geminis") {
     alert(
       "Sueles tener a diario demasiadas dudas a la hora de decidir, Géminis. Piensa hoy que no es positivo que estés pensando todo el tiempo en si hacer o no una cosa por temor a equivocarte."
     );
-  } else if (horo == "cancer") {
+  } else if (horo == "Cancer") {
     alert(
       "No puedes pasar ni un día más infravalorándote, Cáncer, hoy mismo has de ponerle solución."
     );
-  } else if (horo == "leo") {
+  } else if (horo == "Leo") {
     alert(
       "La vida te está mostrando su cara más amable, Leo, y te ofrece a diario muchas oportunidades, algo fundamental para lograr avanzar."
     );
-  } else if (horo == "virgo") {
+  } else if (horo == "Virgo") {
     alert(
       "Estás hoy en un momento muy bueno para estimular tu mente, Virgo. Ahora tienes facilidad para adquirir nuevos conocimientos y tu concentración aumenta a diario."
     );
-  } else if (horo == "libra") {
+  } else if (horo == "Libra") {
     alert(
       "Procura no ser tan despistada, Libra, y pon más atención a diario a lo que tienes entre manos, en especial en el trabajo."
     );
-  } else if (horo == "escorpio") {
+  } else if (horo == "Escorpio") {
     alert(
       "Estás pensando hoy, Escorpio, que a diario la vida te pone a ti las cosas más difíciles que a los demás. Quizá porque en estos días corren ciertos rumores en tu trabajo sobre una posible reducción de personal. No te creas ni una palabra"
     );
-  } else if (horo == "sagitario") {
+  } else if (horo == "Sagitario") {
     alert(
       "Estás pasando por unos momentos de tensión y no dejas de pensar a diario en algo que te atormenta, Sagitario. No aportas nada positivo a tu vida con tanto nerviosismo. Podrías pillar un estrés monumental y no te ayudaría en nada."
     );
-  } else if (horo == "capricornio") {
+  } else if (horo == "Capricornio") {
     alert(
       "Si hoy, Capricornio, estás viviendo una situación complicada que puede ser muy negativa para tu futuro, recuerda que en el pasado algunas personas te orientaron muy bien y te dieron buenos consejos a diario."
     );
-  } else if (horo == "acuario") {
+  } else if (horo == "Acuario") {
     alert(
       "Es momento hoy de empezar a tomarte en serio el tema de la actividad física, Acuario. Recuerda que con sólo apuntarte al gimnasio no resuelves el asunt"
     );
-  } else if (horo == "piscis") {
+  } else if (horo == "Piscis") {
     alert(
       "Tienes que aprender hoy a liberar tus emociones, Piscis, porque si las guardas dentro a diario acabarán perjudicando a tu organismo. "
     );
